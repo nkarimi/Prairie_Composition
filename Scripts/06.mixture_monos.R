7070150a819dc301ea987dfe5e40e7cb95dd61ce
nkarimi <nkarimi@wisc.edu>
2021-07-26 14:27:24 -0500

    Update 06.mixture_monos.R

--- Scripts/06.mixture_monos.R
+++ Scripts/06.mixture_monos.R
@@ -20,8 +20,6 @@ sppCovMat2019 <- read.csv("../DATA/2019_VegetationCover/COVER.sppCovMat.2019.09.
 abund.mix <- sppCovMat2019
 #abund.spp <- intersect(abund.mono$sp, names(abund.mix[5:ncol(abund.mix)])) #127 checks out
 abund.mix.long <- melt(abund.mix, id.vars = c("plot", "year", "type", "Code"), variable.name = "Species")
-
-#drop NA columns - remove rows from df.cover.long for which value = NA (but keeping those with cover = 0)
 abund.mix.long2 <- abund.mix.long %>% drop_na()
 abund.mix.long2 <- split(abund.mix.long2, abund.mix.long2$type)
 
@@ -35,7 +33,6 @@ names(abund.mix.mean) <- c("Species", "mix.cover", "SD_mix", "CoV")
 
 #Merge mono and mixture:
 abund.dat <- merge(abund.mix.mean, abund.mono.mean, by="Species")
-
 abund.dat <- abund.dat %>%
   group_by(Species) %>%
   mutate(Proportional_abundance_in_mixture_relative_to_expectation = mix.cover / (mean_mono/ 15))
@@ -60,25 +57,14 @@ geom_point() + labs(size = "CoV") +
   scale_size_continuous(range = c(0, 6))
 
 p.abund <- p.abund + geom_smooth(method = "lm", show.legend = FALSE) + theme(legend.position="right")
-#p.abund <- p.abund + geom_smooth(method = "lm", formula = y ~ poly(x, 2), show.legend = FALSE) + theme(legend.position="right")
-
 p.abund <- p.abund + geom_text_repel(aes(fontface='italic', color = `Relative competitiveness`),
                          size = 2.5,
-                         segment.size = 0.15, show.legend = FALSE) + theme_bw() +
-  annotate(geom="text", label = "Maianthemum racemosum", fontface = 'italic', x=15, y=7, size = 2.5, colour = "gray") +
-  annotate(geom="text", label ="Liatris scariosa", x=25, y=10.0, fontface = 'italic',size = 2.5, colour = "gray") +
-  annotate(geom="text", label = "Solidago speciosa", x=15, y=8, fontface = 'italic',size = 2.5, colour = "gray") +
-  annotate(geom="text", label = "Teucrium canadense", x=15, y=6, fontface = 'italic',size = 2.5, colour = "gray")
+                         segment.size = 0.15, show.legend = FALSE) + theme_bw() 
 
 lmfit <- lm(`Monoculture abundance` ~ `Proportional abundance in mixture relative to expectation`, data=abund.dat)
 summary(lmfit)
-#abund.dat$Time2 <- abund.dat$`Proportional abundance in mixture relative to expectation`^2
-#quadfit <- lm(`Monoculture abundance` ~ `Proportional abundance in mixture relative to expectation` + Time2, data=abund.dat)
-#summary(quadfit)
-#modelme <- lm(`Proportional abundance in mixture relative to expectation` ~ poly(`Monoculture abundance`, 2), data=abund.dat)
-            
+       
 #ggsave(file="../OUT/mono-mix.expectations.pdf")
-
 
 #####################GET MAX values#####################
 Mono19.06 <- read.csv("../DATA/2019_VegetationCover/Mono.2019.06.Plug.csv", as.is = TRUE)
@@ -136,7 +122,6 @@ abund.dat.max[!grepl("NaN", abund.dat.max$`Proportional abundance in mixture rel
 abund.dat.max[!grepl("Inf", abund.dat.max$`Proportional abundance in mixture relative to expectation`),] -> abund.dat.max
 abund.dat[!grepl("Asclepias_tuberosa", abund.dat$Species),] -> abund.dat.max
 
-
 p.abundmax <- ggplot(abund.dat.max,
                   aes(x = `Monoculture abundance`,
                       y = `Proportional abundance in mixture relative to expectation`,
@@ -145,26 +130,16 @@ p.abundmax <- ggplot(abund.dat.max,
   scale_size_continuous(range = c(0, 10))
 
 p.abundmax <- p.abundmax + geom_smooth(method = 'lm', show.legend = FALSE) + theme(legend.position="right")
-
 p.abundmax <- p.abundmax + geom_text_repel(aes(fontface='italic', color = `Relative competitiveness`),
                                      size = 2.5,
                                      segment.size = 0.15, show.legend = FALSE) + theme_bw()
 
 #ggsave(file="../OUT/mono-mix.expectations.png")
 
-#color dots based on family, greyscale?
-#check for slope signifncance !!
-
-
 #########################################
 #SLOPEPLOT
 ####RESCALE FOR SLOPEPLOTS ONLY
-        #need to rescale data 
-        #rescale relative to mixtures or just make rank order plot
-        #use the mean for mixtures and each plot should have its own line to monoculture
-        #abund.dat$mono.log <- log(abund.dat$mean_value_mono)
-        #abund.dat$mix.log <- log(abund.dat$mean_value_mix)
-        is.na(abund.dat)<-sapply(abund.dat, is.infinite) #Does this make sense? Convert Inf or NA to 0
+        is.na(abund.dat)<-sapply(abund.dat, is.infinite)
         abund.dat[is.na(abund.dat)] <- 0
         
         #MEAN SCALING
@@ -205,82 +180,4 @@ p.abundmax <- p.abundmax + geom_text_repel(aes(fontface='italic', color = `Relat
                   plot.margin = unit(c(1,2,1,2), "cm"))
         
         p
-        ggsave(filename = "../OUT/scaled.mix-mono.SlopePlot.png", width = 8, height=16)
-        
-        
-        abund.dat2   
-#########################################
-#RANK ORDER OVER TIME SERIES
-#http://r-statistics.co/Top50-Ggplot2-Visualizations-MasterList-R-Code.html
-#test on df.cover
-
-library(ggplot2)
-library(lubridate)
-library(CGPfunctions)
-theme_set(theme_bw())
-
-abund.dat2$year <- as.factor(abund.dat2$year)
-abund.dat2$logMix <- log(abund.dat2$mean_value_mix) #Log tranform cover
-abund.dat2$logMix <- round(abund.dat2$logMix, 2)
-
-#newggslopegraph(newcancer, Year, Survival, Type)
-#newggslopegraph(abund.dat2, year, logMix, Species, Title = "Mean Cover",
-#                SubTitle = "Plug Plots",
-#                Caption = NULL,YTextSize = 2.5,
-#)
-#ggsave(filename = "../OUT/meanCover.slopePlot.png", width = 8, height=16)
-
-#versus
-df.coverlong <- melt(df.cover, id.vars = c("plot", "year", "type", "Code"), variable.name = "Species")
-df.coverlong  <- filter(df.coverlong, value !="NA")
-
-df.mean.cover <- df.coverlong %>%
-  group_by(Species, year, type) %>%
-  summarise(Mean =mean(value), Sd= sd(value))
-
-df.mean.cover <- split(df.mean.cover, df.mean.cover$type)
-as.factor(df.mean.cover$Plug$year) -> df.mean.cover$Plug$year
-df.mean.cover$Plug<- as.data.frame(df.mean.cover$Plug)
-
-#join traits.mat to df.mean.cover$Plug by Species
-df.mean.cover$Plug -> copy
-copy <-  merge(df.mean.cover$Plug, traits.mat[, c("Species", "SDMC", "SLA", "vegetativeHeight")], by="Species")
-copy$logMean <- log(copy$Mean) #Log tranform cover
-library(directlabels)
-copy[1:60,] -> copy2
-
-ggplot(copy2, aes(x=year)) + 
-  geom_line(aes(y=logMean, col=vegetativeHeight, group=Species), size=2)+
-  labs(title="Time Series of Mean Cover", 
-       subtitle="Colored by Vegetative Height", 
-       caption="", 
-       y="(log)Mean Cover by Species", 
-       color=NULL) +
-  geom_dl(aes(label= Species, y=logMean), method=list(dl.combine("first.points", "last.points"), cex = 0.5)) 
-  
-  #geom_label_repel(aes(label = Species, y=logMean),
-  #                 nudge_x = 1,
-  #                 na.rm = TRUE)
-#ggsave(filename = "../OUT/logmeanCover.slopePlot.SDMC.png", width = 8, height=11)
-
-#make a list of species to plot (based on significance for somthing and then call that in ggplot)
-test.specieslist <- c("Agastache_nepetoides", "Agastache_scrophulariifolia")
-
-#ERRORS by plotting only 2 years not if call a species specifically as below
-ggplot(copy[copy$Species==test.specieslist,], aes(x=year)) +
-  geom_line(aes(y=logMean, col=SDMC, group=Species), size=2)+
-  labs(title="Time Series of Mean Cover", 
-       subtitle="Colored by SDMC", 
-       caption="", 
-       y="(log)Mean Cover by Species", 
-       color=NULL) 
-
-ggplot(copy[copy$Species==c("Agastache_nepetoides"),], aes(x=year)) +
-  geom_line(aes(y=logMean, col=SDMC, group=Species), size=2)+
-  labs(title="Time Series of Mean Cover", 
-       subtitle="Colored by SDMC", 
-       caption="", 
-       y="(log)Mean Cover by Species", 
-       color=NULL) 
-
-
+#        ggsave(filename = "../OUT/scaled.mix-mono.SlopePlot.png", width = 8, height=16)
